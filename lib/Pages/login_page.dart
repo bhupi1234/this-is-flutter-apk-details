0ea812795ddabd18246c1abb7ea0ba6b2e9259a7
bhupender <hellobhupenderofficial@gmail.com>
2022-11-21 18:15:53 +0530

    day 6 is completed button animation

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/Routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -12,7 +21,7 @@ class LoginPage extends StatelessWidget {
             height: 20.0,
           ),
           Text(
-            "Welcome",
+            "Welcome $name",
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
@@ -32,6 +41,10 @@ class LoginPage extends StatelessWidget {
                   hintText: "Enter Username",
                   labelText: "Username",
                 ),
+                onChanged: (value) {
+                  name = value;
+                  setState(() {});
+                },
               ),
               TextFormField(
                 obscureText: true,
@@ -43,13 +56,47 @@ class LoginPage extends StatelessWidget {
               SizedBox(
                 height: 10.0,
               ),
-              ElevatedButton(
-                child: Text("Login"),
-                style: TextButton.styleFrom(minimumSize: Size(100, 40)),
-                onPressed: () {
+
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    changeButton = true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
                   Navigator.pushNamed(context, MyRoutes.homeRoute);
                 },
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  width: changeButton ? 50 : 100,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: changeButton
+                      ? Icon(Icons.done, color: Colors.white)
+                      : Text(
+                          "Login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      //  shape: changeButton ?
+                      //BoxShape.circle :
+                      // BoxShape.rectangle,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8)),
+                ),
               ),
+
+              // ElevatedButton(
+              //   child: Text("Login"),
+              //   style: TextButton.styleFrom(minimumSize: Size(100, 40)),
+              //   onPressed: () {
+              //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+              //   },
+              //  ),
             ]),
           ),
         ]),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/Routes.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 
